8e7d7b267912a53eac2b5f58c764c204f186ab8a
Marvens Mertune <marvensmertune@gmail.com>
2022-09-20 14:18:05 -0400

    Initial Commit

---
+++ seabnb_server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs")
+
+app.get("/", (req,res)=>{
+    res.send("Heyyo")
+})
+
+app.listen(3000, () => {
+  console.log("Listening on port");
+});
