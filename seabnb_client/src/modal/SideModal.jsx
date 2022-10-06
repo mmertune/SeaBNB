fb1a7a5c121422b419e742a400e1a81e4e2b004f
Marvens Mertune <marvensmertune@gmail.com>
2022-10-06 17:47:52 -0400

    Added Side modal and implemeted functionality

--- seabnb_client/src/modal/SideModal.jsx
+++ seabnb_client/src/modal/SideModal.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "../assets/css/Modal.css"
+import {toggleSideModal} from "../redux/modalSlice"
 
 const SideModal = () => {
+  const {sideVisible} = useSelector(store=>store.modal)
+   const dispatch = useDispatch()
   return (
-    <div className="sidemodal modal">
-      <div className="modal_background"></div>
-      <aside>
-        
+    <div className={`sidemodal modal ${sideVisible ? "" : "modal_hide"}`}>
+      <div className="modal_background" onClick={()=>dispatch(toggleSideModal())}></div>
+      <aside className="sidemodal_sideContainer">
+        <button onClick={()=>dispatch(toggleSideModal())}>Add Campground</button>
       </aside>
     </div>
   );
